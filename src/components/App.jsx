af3b40e0940bc1c9a3d31bc955db68d6c880520d
Sagor <mbrsagor@gmail.com>
2020-12-26 01:26:47 +0600

    Add profile component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Profile from './profile';
 
 class App extends React.Component {
     render() {
         return (
-            <h1>Welcoe to our Homepage</h1>
+            <div class="container">
+                <div className="row">
+                    <div className="col-md-6 offset-3">
+                        <h1>Welcoe to our Homepage</h1>
+                        <Profile />
+                    </div>
+                </div>
+            </div>
         )
     }
 }
---
+++ src/components/profile/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+class Profile extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h3>I'm Sagor</h3>
+                <p>I'm full-stack software Engineer</p>
+                <p>Skills: Python, JavaScript, Dart, React</p>
+                <p>Find me on socail</p>
+                <ul>
+                    <li><a href="https//linkedin.com/mbrsagor">Linkedin</a></li>
+                    <li><a href="https//github.com/mbrsagor">GitHub</a></li>
+                    <li><a href="https//facebook.com/mbrsagor">Facebook</a></li>
+                    <li><a href="https//twitter.com/mbrsagor">Twitter</a></li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Profile;
