37e320bd48effd254eee483730120582f2c3c303
Sagor <mbrsagor@gmail.com>
2020-12-26 01:02:20 +0600

    Unused files deleted

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <h1>Welcoe to our Homepage</h1>
+        )
+    }
+}
+
+export default App;
