a35f64fa869bc0aa43dbbd60635fe2c40803cf42
Sagor <mbrsagor@gmail.com>
2020-12-27 19:31:32 +0600

    Formik form

--- src/components/formik/form.jsx
+++ src/components/formik/form.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Formik, Form, Field} from 'formik';
 
 const MyForm = (props) => {
+    console.log(props.errors)
     return (
        <Form>
             <div className="form-group">
--- src/components/formik/index.jsx
+++ src/components/formik/index.jsx
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
-import { Formik, Form, Field } from 'formik';
-import MyForm from './form'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+// import MyForm from './form'
+
+import * as Yup from 'yup';
+
+const myValidtionsScheme = Yup.object().shape({
+    name: Yup.string().required('Name is required').min(3, 'Minumum 3 chr').max(15, 'Maximum 15 char'),
+    email: Yup.string().email('Enter valid email address').required('Email must be required'),
+    password: Yup.string('Enter password').required('Password must be 15 char')
+});
 
 class MyFormik extends Component {
     render() {
@@ -13,9 +21,12 @@ class MyFormik extends Component {
                         console.log(values);
                         console.log(formikBag);
                     }}
-                    component={MyForm}
-                    />
-{/*                     
+                    validationSchema={myValidtionsScheme}
+                    // validateOnBlur={true}
+                    // validateOnChange={false}
+                    // component={MyForm}
+                    >
+                    
                     {(props) => {
                         // console.log(props);
                         return <Form>
@@ -24,6 +35,8 @@ class MyFormik extends Component {
                                 className="form-control mb-2"
                                 name="name"
                                 placeholder="Enter your name" />
+                                {/* {props.errors.name && props.touched.name && <div className="text-warning">{props.errors.name}</div>} */}
+                                <ErrorMessage name="name" />
                             </div>
                             <div className="form-group">
                                 <Field
@@ -31,6 +44,8 @@ class MyFormik extends Component {
                                 name="email"
                                 type="email"
                                 placeholder="Enter your email" />
+                                {/* {props.errors.email && props.touched.email && <div>{props.errors.email}</div>} */}
+                                <ErrorMessage name="email" />
                             </div>
                             <div className="form-group">
                                 <Field
@@ -38,11 +53,13 @@ class MyFormik extends Component {
                                 className="form-control mb-2"
                                 name="password"
                                 placeholder="**********" />
+                                {/* {props.errors.password && props.touched.password && <div>{props.errors.password}</div>} */}
+                                <ErrorMessage name="password" />
                             </div>
                             <button type="submit" className="btn btn-success btn-sm">save</button>
                         </Form>
                     }}
-                </Formik> */}
+                </Formik>
             </div>
         );
     }
