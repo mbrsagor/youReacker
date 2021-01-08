6250fc8e71f4447fe917eed7bf78e0a7ed4f7843
Sagor <mbrsagor@gmail.com>
2021-01-08 23:17:38 +0600

    Counter simple reducer

--- src/components/hooks/index.jsx
+++ src/components/hooks/index.jsx
@@ -1,14 +1,33 @@
-import React, {useContext} from 'react';
+import React, {useReducer} from 'react';
 
-const Hooks = () => {
 
-    // const {count, setCount} = useContext(0)
+const initalData = 0;
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'increment':
+            return state + 1;
+        case 'decrement':
+            return state - 1;
+        case 'reset':
+            return 0;
+        default: return state;
+    }
+}
+
+const MyReducer = props => {
+
+    const [count, dispatch] = useReducer(reducer, initalData);
 
     return (
         <div>
-            <h2>I'm calling from Hooks</h2>
+            <h2>I am use Reducer</h2>
+            <h3>Count: {count}</h3>
+            <button onClick={() => dispatch({type: 'increment'})} className="btn btn-success btn-sm mr-2">+</button>
+            <button onClick={() => dispatch({type: 'decrement'})} className="btn btn-success btn-sm mr-2">-</button>
+            <button onClick={() => dispatch({type: 'reset'})} className="btn btn-danger btn-sm mr-2">(0)</button>
         </div>
     );
 }
 
-export default Hooks;
+export default MyReducer;
